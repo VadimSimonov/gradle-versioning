0360e64853a50edb19c06e73cc7bf8251a37ac58
Vadim Simonov <vsimonov@glosav.ru>
2019-07-11 17:51:39 +0300

    TOOLS-1 Реализовать плагин gradle-versioning

--- src/main/java/ru/glosav/tools/gradle/versioning/VersioningPlugin.java
+++ src/main/java/ru/glosav/tools/gradle/versioning/VersioningPlugin.java
@@ -1,20 +1,19 @@
 package ru.glosav.tools.gradle.versioning;
 
-import net.nemerosa.versioning.VersionInfo;
 import net.nemerosa.versioning.VersioningExtension;
 import net.nemerosa.versioning.tasks.VersionDisplayTask;
 import net.nemerosa.versioning.tasks.VersionFileTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.tasks.bundling.Jar;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.Date;
-import java.util.Map;
-import java.util.TreeMap;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.util.Collections.singletonMap;
 
 /**
  * @author Grigory Panin
@@ -22,14 +21,17 @@ import java.util.TreeMap;
  * Плагин версионирования для Gradle
  */
 public class VersioningPlugin implements Plugin<Project> {
-    private static final Logger logger = LoggerFactory.getLogger(VersioningPlugin.class);
+    private static final List<String> plugins = Arrays.asList("java", "idea", "org.springframework.boot");
 
     @Override
     public void apply(Project project) {
+        /*for (String plugin : plugins) {
+            project.apply(singletonMap("plugin", plugin));
+        }*/
+
         VersioningPluginExtension extension = project.getExtensions()
             .create("customManifestProperties", VersioningPluginExtension.class);
-        System.out.println("inside VersioningPlugin");
-        logger.info("logger inside VersioningPlugin");
+
         String buildDir = System.getProperty("user.dir") + "/build";
         project.setBuildDir(new File(buildDir));
         project.getExtensions().create("versioning", VersioningExtension.class, project);
@@ -37,23 +39,22 @@ public class VersioningPlugin implements Plugin<Project> {
         project.getTasks().create("versionFile", VersionFileTask.class);
 
         VersioningExtension versioning = (VersioningExtension) project.getExtensions().getByName("versioning");
-        VersionInfo info = versioning.getInfo();
-        logger.info("\nversioning info= " + info.toString() + "\n");
-        System.out.println("\nversioning info= " + info.toString() + "\n");
 
         project.task("customManifest")
-            .doLast(task -> {
-                logger.info("logger inside customManifest task");
-                System.out.println("inside customManifest task");
-                Jar jar = (Jar) project.getTasks().getByName(JavaPlugin.JAR_TASK_NAME);
+            .doFirst(task -> {
+                Jar jar = null;
                 Map<String, Object> properties = new TreeMap<>();
                 if (extension.getStartClass() != null) {
                     properties.put("Start-Class", extension.getStartClass());
+                    jar = (Jar) project.getTasks().getByName("bootJar");
+                } else {
+                    jar = (Jar) project.getTasks().getByName(JavaPlugin.JAR_TASK_NAME);
                 }
+
                 properties.put("Build-Jdk", "" + System.getProperty("java.version") + " " + System.getProperty("java.vendor") + " " + System.getProperty("java.vm.version"));
                 properties.put("Build-OS", "" + System.getProperty("os.name") + " " + System.getProperty("os.arch") + " " + System.getProperty("os.version"));
                 properties.put("Built-By", System.getProperty("user.name"));
-                properties.put("Build-Timestamp", new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date()));
+                properties.put("Build-Timestamp", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date()));
                 properties.put("Build-Scm", versioning.getInfo().getScm());
                 properties.put("Build-BranchType", versioning.getInfo().getBranchType());
                 properties.put("Build-BranchId", versioning.getInfo().getBranchId());
@@ -65,18 +66,18 @@ public class VersioningPlugin implements Plugin<Project> {
                 properties.put("Build-Display", versioning.getInfo().getDisplay());
                 properties.put("Build-Dirty", versioning.getInfo().getDirty());
                 properties.put("Build-Tag", versioning.getInfo().getTag() == null ? "undefined" : versioning.getInfo().getTag());
-                properties.put("Created-Tag", versioning.getInfo().getScm());
-
-                // TODO attributes
+                properties.put("Created-Tag", project.getVersion().toString());
 
                 jar.manifest(manifest -> {
                     manifest.attributes(properties);
                 });
             });
-    }
 
-    private void versionProp(VersioningExtension versioning, String buildDir) {
-        File file = new File(buildDir, "version.properties");
+        Task versionFile = project.getTasks().getByName("versionFile");
+        VersionFileTask version = (VersionFileTask) versionFile;
+/*        version.dependsOn(project.getTasks().getByName(JAR_TASK_NAME));
+        version.dependsOn(project.getTasks().getByName("customManifest"));*/
+        version.run();
 
     }
 }
